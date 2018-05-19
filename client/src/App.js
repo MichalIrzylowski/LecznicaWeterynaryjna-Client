251820d3db3fbdf8c42b9c8e7b127a553b40038f
unknown <michal.irzylowski@gmail.com>
2018-05-19 17:52:22 +0200

    Begining

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Landing Page</h1>
+        <button>Login site</button>
+      </div>
+    );
+  }
+}
+
+export default App;
